dc8af6cf46e3c54cb1583ee379b0f35e651782d6
crash4star <47321264+crash4star@users.noreply.github.com>
2020-03-19 16:42:07 +0300

    feat: complete towel-sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix == undefined) {
+        return [];
+    } else {
+        const arr = matrix.map((item, i, a) => {
+            if (i % 2 !== 0) {
+                return item.reverse();
+            } else {
+                return item;
+            }
+        });
+
+        let sortArr = [];
+
+        arr.forEach((item, i, a) => {
+            item.forEach(el => sortArr.push(el));
+        });
+
+        return sortArr;
+    }
 }
